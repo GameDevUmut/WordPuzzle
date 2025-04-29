d5176ce81f9fc8a903e820432b979221612c6fda
Umut Can ERYILDIZ <uwsiletisim@gmail.com>
2025-04-29 03:45:41 +0300

    Implement observer pattern

---
+++ Assets/_Scripts/General/ObserverBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Interfaces;
+using UnityEngine;
+
+namespace General
+{
+    public class ObserverBase : MonoBehaviour, IObserver
+    {
+        protected ISubject Subject { get; set; }
+
+        public virtual void ObserveSubject(ISubject subject)
+        {
+            Subject = subject;
+
+            if (Subject != null)
+                Subject.AddObserver(this);
+        }
+
+        protected void OnDestroy()
+        {
+            if (Subject != null)
+                Subject.RemoveObserver(this);
+        }
+
+        public virtual void OnNotify(GameObject entity, ObserverEventType eventType)
+        {
+        }
+    }
+}
---
+++ Assets/_Scripts/Interfaces/ISubject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public interface ISubject
+    {
+        void AddObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers(GameObject entity, ObserverEventType eventType);
+    }
+}
