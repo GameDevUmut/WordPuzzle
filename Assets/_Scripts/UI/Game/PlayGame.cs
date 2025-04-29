b263f7a8a4c1e916ed75fe98592c0f83bb65cfdf
Umut Can ERYILDIZ <uwsiletisim@gmail.com>
2025-04-29 22:13:44 +0300

    Implement resizable, flexible grid for responsive gameplay on all screens

--- Assets/_Scripts/UI/Game/PlayGame.cs
+++ Assets/_Scripts/UI/Game/PlayGame.cs
@@ -1,11 +1,18 @@
+using GameCore.GridSystem;
 using Interfaces;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using VContainer;
 
 namespace UI.Main
 {
     public class PlayGame : MonoBehaviour
     {
+        [SerializeField] private TMP_InputField xInputField;
+        [SerializeField] private TMP_InputField yInputField;
+        
+        
         private ISceneLoadService _sceneLoadService;
 
         [Inject]
@@ -13,9 +20,17 @@ namespace UI.Main
         {
             _sceneLoadService = sceneLoadService;
         }
+        
+        private Vector2 GetInputvalues()
+        {
+            int x = int.Parse(xInputField.text);
+            int y = int.Parse(yInputField.text);
+            return new Vector2(x, y);
+        }
     
         public void OnPlayGamePressed()
         {
+            GridManager.GridSize = GetInputvalues();
             _sceneLoadService.Load(ISceneLoadService.SceneName.GameScene);
         }
     }
